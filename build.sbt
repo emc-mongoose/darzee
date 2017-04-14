c63434663c9803c7c6bc57ad790bf66a150a2ea6
Andrey Kurilov <andrey.kurilov@emc.com>
2017-04-15 00:54:37 +0300

    save

--- build.sbt
+++ build.sbt
@@ -13,9 +13,11 @@ scalaSource in Test := baseDirectory.value / "test"
 resolvers += DefaultMavenRepository
 
 libraryDependencies ++= Seq(
-  "org.webjars" % "requirejs" % "2.3.3",
-  "org.webjars" %% "webjars-play" % "2.5.0-4",
-  "org.webjars" % "font-awesome" % "4.7.0",
-  "org.webjars.bower" % "dojo" % "1.12.1",
-  "org.webjars.bower" % "dijit" % "1.12.1"
+	"org.webjars" % "requirejs" % "2.3.3",
+	"org.webjars" %% "webjars-play" % "2.5.0-4",
+	"org.webjars" % "font-awesome" % "4.7.0",
+	"org.webjars.bower" % "dojo" % "1.12.1",
+	"org.webjars.bower" % "dijit" % "1.12.1",
+	"org.webjars.bower" % "dojox" % "1.12.1", // looks like it's required by gridx
+	"org.webjars" % "gridx" % "1.3.9"
 )
---
+++ public/javascripts/main.js
@@ -0,0 +1,47 @@
+require(
+
+	[
+		"dojo/parser",
+		"dojo/store/Memory",
+
+		"dijit/layout/ContentPane",
+		"dijit/layout/LayoutContainer",
+		"dijit/layout/TabContainer",
+		"dijit/Fieldset",
+		"dijit/Menu",
+		"dijit/MenuItem",
+		"dijit/form/Button",
+		"dijit/Editor",
+		"dijit/tree/ObjectStoreModel",
+		"dijit/Tree",
+
+		"dojox/editor/plugins/FindReplace",
+		"dojox/editor/plugins/Save",
+		"dojox/editor/plugins/StatusBar",
+
+		"gridx/Grid",
+		"gridx/core/model/cache/Sync",
+
+		"dojo/domReady!"
+	],
+
+	function(parser) {
+
+		parser.parse();
+
+		/*var dashboardCurrentRunInfoData = new Memory(
+			{
+				data: [
+					{ id: 1, name: "Mongoose Version", value: "3.4.5" },
+					{ id: 4, name: "Controller Host", value: "host1"},
+					{ id: 2, name: "Configuration", value: "link" },
+					{ id: 3, name: "Scenario", value: "link" }
+				]
+			}
+		);
+		var dashboardCurrentRunInfoFields = [
+			{ id: "name", field: "name", name: "Name", width: "100px" },
+			{ id: "value", field: "value", name: "Value" }
+		];*/
+	}
+);
