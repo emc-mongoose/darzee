ef9e8ca0161918897b14d4f97c472c2da622ef3b
Alina <alina.alexandrovna.sh@gmail.com>
2018-10-09 16:06:41 +0300

    JS file and "nailed" versions in index.html
    “Signed-off-by: Alina Pereskokova alina.alexandrovna.sh@gmail.com” 
    
    Developer's Certificate of Origin 1.1 
    
    By making a contribution to this project, I certify that: 
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or 
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or 
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it. 
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.

--- src/main/webapp/js/requirejs/conf.js
+++ src/main/webapp/js/requirejs/conf.js
@@ -1,7 +1,6 @@
 requirejs.config({
 
 	"paths": {
-		// "bootstrap": ["/webjars/bootstrap/4.1.3/js/bootstrap", "js/bootstrap"],
         "bootstrap": ["/webjars/bootstrap/" + depVersion.bootstrap + "/js/bootstrap", "js/bootstrap"],
 		"d3js": ["/webjars/d3js/" + depVersion.d3js + "/d3", "d3"],
 		"validate-js": ["/webjars/validate.js/" + depVersion.validatejs + "/validate"],
