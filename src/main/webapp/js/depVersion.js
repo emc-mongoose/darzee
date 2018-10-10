d36871a8bfbc4ae38a1d5e738a08352ae0c18e74
Alina <alina.alexandrovna.sh@gmail.com>
2018-10-10 10:55:01 +0300

    add versions
    “Signed-off-by: Alina Pereskokova alina.alexandrovna.sh@gmail.com” 
    
    Developer's Certificate of Origin 1.1 
    
    By making a contribution to this project, I certify that: 
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or 
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or 
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it. 
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.

---
+++ src/main/webapp/js/depVersion.js
@@ -0,0 +1,11 @@
+let depVersion = {
+    "bootstrap": "4.1.3",
+    "d3js": "3.5.3",
+    "validatejs": "0.8.0",
+    "jquery": "3.0.0",
+    "handlebars": "4.0.2",
+    "pace": "1.0.2",
+    "text": "2.0.15",
+    "uiselectable": "1.11.4",
+    "requirejs": "2.1.22"
+}
