57e8aa6c6224b18df6ee65e620096c9a4ef59bfc
Alina <alina.alexandrovna.sh@gmail.com>
2018-10-24 17:18:50 +0300

    refactoring structure
    “Signed-off-by: Alina Pereskokova alina.alexandrovna.sh@gmail.com” 
    
    Developer's Certificate of Origin 1.1 
    
    By making a contribution to this project, I certify that: 
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or 
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or 
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it. 
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.

---
+++ out-tsc/spec/app/control-tabs/control-tabs.component.js
@@ -0,0 +1,27 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Component } from '@angular/core';
+var ControlTabsComponent = /** @class */ (function () {
+    function ControlTabsComponent() {
+    }
+    ControlTabsComponent.prototype.ngOnInit = function () {
+    };
+    ControlTabsComponent = __decorate([
+        Component({
+            selector: 'app-control-tabs',
+            templateUrl: './control-tabs.component.html',
+            styleUrls: ['./control-tabs.component.css']
+        }),
+        __metadata("design:paramtypes", [])
+    ], ControlTabsComponent);
+    return ControlTabsComponent;
+}());
+export { ControlTabsComponent };
+//# sourceMappingURL=control-tabs.component.js.map
---
+++ out-tsc/spec/app/homepage/homepage.module.spec.js
@@ -0,0 +1,11 @@
+import { HomepageModule } from './homepage.module';
+describe('HomepageModule', function () {
+    var homepageModule;
+    beforeEach(function () {
+        homepageModule = new HomepageModule();
+    });
+    it('should create an instance', function () {
+        expect(homepageModule).toBeTruthy();
+    });
+});
+//# sourceMappingURL=homepage.module.spec.js.map
