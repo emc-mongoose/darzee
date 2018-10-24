8bb45f6853e26b9e9ff48a13a488ef3c8ccb1d77
Alina <alina.alexandrovna.sh@gmail.com>
2018-10-24 13:41:02 +0300

    some corrections
    “Signed-off-by: Alina Pereskokova alina.alexandrovna.sh@gmail.com” 
    
    Developer's Certificate of Origin 1.1 
    
    By making a contribution to this project, I certify that: 
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or 
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or 
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it. 
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from "./homepage/homepage.component";
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
