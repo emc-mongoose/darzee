425872392168ea463539ea21cf3fd6a1be568472
Andrey Koltsov <a99920641@gmail.com>
2018-10-29 07:43:35 +0300

    Add stat button for scenarios. Add Save button for scenarios. Add *open* button for scenarios. By making a contribution to this project, I certify that:
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.
    
    Signed-off-by: Andrey Koltsov a99920641@gmail.com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContorlPageModule } from './contorl-page/contorl-page.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    // NOTE: Custom modules
+    ContorlPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
