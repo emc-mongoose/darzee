f41eb6ea405514e542f8ec1b79cd99d5ff307eea
Andrey Koltsov <a99920641@gmail.com>
2018-10-29 08:35:01 +0300

    Add  file input & basic file processing @ Control page. By making a contribution to this project, I certify that:
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.
    
    Signed-off-by: Andrey Koltsov a99920641@gmail.com

--- src/app/contorl-page/scenarios/scenarios.component.ts
+++ src/app/contorl-page/scenarios/scenarios.component.ts
@@ -12,4 +12,23 @@ export class ScenariosComponent implements OnInit {
   ngOnInit() {
   }
 
+  processFile(event) { 
+    if (event.target.files.length == 0) {
+      console.log("File hasn't been selected.");
+      return
+   }
+     let file: File = event.target.files[0];
+     console.log("Contet: ", file.name)
+
+     let fileReader = new FileReader();
+     fileReader.onload = () => {
+      console.log(fileReader.result);
+    };
+    fileReader.readAsText(file);
+
+
+     // after here 'file' can be accessed and used for further process
+  }
+  
+
 }
