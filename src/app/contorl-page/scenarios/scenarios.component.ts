57d02b36b2cef82c37422c6ba17ba87a8b92e50a
Alina <alina.alexandrovna.sh@gmail.com>
2018-10-30 17:08:38 +0300

    some refactoring
    “Signed-off-by: Alina Pereskokova alina.alexandrovna.sh@gmail.com”
    
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.

--- src/app/contorl-page/scenarios/scenarios.component.ts
+++ src/app/contorl-page/scenarios/scenarios.component.ts
@@ -9,38 +9,38 @@ export class ScenariosComponent implements OnInit {
 
   private fileContent: string | ArrayBuffer;
   private processingFile: File;
-  constructor() { 
-    this.fileContent = ""
+  constructor() {
+    this.fileContent = '';
     this.processingFile = null;
   }
 
   ngOnInit() {
   }
 
-  processFile(event) { 
-    if (event.target.files.length == 0) {
-      console.log("File hasn't been selected.");
-      return
+  processFile(event) {
+    if (event.target.files.length === 0) {
+      console.log('File has not been selected.');
+      return;
    }
    this.processingFile = event.target.files[0];
-     let fileReader = new FileReader();
+     const fileReader = new FileReader();
      fileReader.onload = () => {
       console.log(fileReader.result);
       this.fileContent = fileReader.result;
     };
     fileReader.readAsText(this.processingFile);
   }
-  
-  onStartBtnClicked() { 
-    alert("Mangoose started.");
+
+  onStartBtnClicked() {
+    alert('Mongoose started.');
   }
-  
+
   onSaveBtnClicked() {
-    if ((this.processingFile != null) && (this.fileContent.toString() != "")) { 
-      alert("File has been saved.");
-    } else { 
-      alert("Nothing to be saved.");
+    if ((this.processingFile != null) && (this.fileContent.toString() !== '')) {
+      alert('File has been saved.');
+    } else {
+      alert('Nothing to be saved.');
     }
-  }  
+  }
 
 }
