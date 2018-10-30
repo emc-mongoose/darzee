a8d0c70a55d858792533fdccf381987e68deb1d1
Andrey Koltsov <a99920641@gmail.com>
2018-10-30 16:04:00 +0300

    Remove *angular@material* package. By making a contribution to this project, I certify that:
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.
    
    Signed-off-by: Andrey Koltsov a99920641@gmail.com

--- src/app/contorl-page/contorl-page.module.ts
+++ src/app/contorl-page/contorl-page.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ControlPageRootComponent } from './control-page-root/control-page-root.component';
-import { MatTabsModule } from '@angular/material/tabs';
 import { ControlNavbarComponent } from './control-navbar/control-navbar.component';
 import { ScenariosComponent } from './scenarios/scenarios.component';
 
 @NgModule({
   imports: [
-    CommonModule,
-    MatTabsModule
+    CommonModule
   ],
   declarations: [ControlPageRootComponent, ControlNavbarComponent, ScenariosComponent],
   exports: [ControlPageRootComponent]
