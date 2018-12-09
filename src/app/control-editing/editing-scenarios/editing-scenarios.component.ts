fccd51306aa51b644a828c67f4129c7670937fda
Andrey Koltsov <a99920641@gmail.com>
2018-12-10 01:41:23 +0300

    Customize the appearence of JSON editor. By making a contribution to this project, I certify that:
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.
    
    Signed-off-by: Andrey Koltsov a99920641@gmail.com

--- src/app/control-editing/editing-scenarios/editing-scenarios.component.ts
+++ src/app/control-editing/editing-scenarios/editing-scenarios.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IpAddressService } from 'src/app/ip-address.service';
 import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
 
@@ -9,14 +9,15 @@ import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
   styleUrls: ['./editing-scenarios.component.css']
 })
 export class EditingScenariosComponent implements OnInit {
+  @ViewChild(JsonEditorComponent) editor: JsonEditorComponent;
+
   public editorOptions: JsonEditorOptions;
   public data: any;
 
   public fileContent: string | ArrayBuffer;
-  constructor(private service: IpAddressService) { 
 
+  constructor(private service: IpAddressService) { 
     this.fileContent = service.fileContent;
-    
     this.configureJsonEditor();
   }
 
@@ -27,8 +28,34 @@ export class EditingScenariosComponent implements OnInit {
   // NOTE: Private methods
   private configureJsonEditor() {
     this.editorOptions = new JsonEditorOptions()
+
+    // NOTE: JSON Editor could be customized using the following fields: 
+    // ... this.editorOptions.mode = 'code'; - it'd customize the displaying of actual JSON; ...
+    // ... ... avaliable modes are: code', 'text', 'tree', 'view'
+    // ... this.editorOptions.schema = schema; - it'd customize the displaying of JSON editor 
     this.editorOptions.modes = ['code', 'text', 'tree', 'view']; // set all allowed modes
-    this.data = {"products":[{"name":"car","product":[{"name":"honda","model":[{"id":"civic","name":"civic"},{"id":"accord","name":"accord"},{"id":"crv","name":"crv"},{"id":"pilot","name":"pilot"},{"id":"odyssey","name":"odyssey"}]}]}]}
+    this.data = {
+      products: [{
+        name: 'car',
+        product: [{
+          name: 'honda',
+          model: [
+            { id: 'civic', name: 'civic' },
+            { id: 'accord', name: 'accord' },
+            { id: 'crv', name: 'crv' },
+            { id: 'pilot', name: 'pilot' },
+            { id: 'odyssey', name: 'odyssey' }
+          ]
+        }]
+      }]
+    };
+  
+   
+    this.editorOptions.statusBar = false;
+    this.editorOptions.onChange = () =>  {
+      console.log("Data has been changed:");
+      console.log(this.editor.get()); 
+    }
   }
 
 }
