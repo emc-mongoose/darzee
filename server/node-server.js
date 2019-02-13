0f594fd488c3b04881526b3cca761a6cabcf7e35
Andrey Koltsov <a99920641@gmail.com>
2019-02-13 22:14:15 +0300

    Set default value for mongoose console serving port @ server configuration.

--- server/node-server.js
+++ server/node-server.js
@@ -1,11 +1,13 @@
+const MONGOOSE_CONSOLE_DEFAULT_PORT = 8080;
 var express = require('express');
 var app = express();
 
 var path = __dirname + '';
-var port = 8080;
+var port = process.env.CONSOLE_PORT || MONGOOSE_CONSOLE_DEFAULT_PORT;
 
 app.use(express.static(path));
 app.get('*', function(req, res) {
     res.sendFile(path + '/index.html');
 });
 app.listen(port);
+
