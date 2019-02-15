5e6758be6ceef4156ca6dab9cdacc6f7e7d74a4c
Andrey Koltsov <a99920641@gmail.com>
2019-02-15 13:03:23 +0300

    Add implicit declaration for Mongoose Run Record class' fields.

--- src/app/core/models/run-record.model.ts
+++ src/app/core/models/run-record.model.ts
@@ -3,6 +3,18 @@ import { MongooseRunStatus } from '../mongoose-run-status';
 
 export class MongooseRunRecord { 
 
-    constructor(status: MongooseRunStatus,  startTime: String, nodes: String[],  duration: RunDuration, comment: String) { }
+    public status: MongooseRunStatus;
+    public startTime: String;
+    public nodes: String[];
+    public duration: RunDuration;
+    public comment: String;
+
+    constructor(status: MongooseRunStatus,  startTime: String, nodes: String[],  duration: RunDuration, comment: String) { 
+        this.status = status;
+        this.startTime = startTime;
+        this.nodes = nodes;
+        this.duration = duration;
+        this.comment = comment;
+    }
 
 }
