f986d6129c8c50b193aacf9a23de1f11be40bb34
Andrey Koltsov <a99920641@gmail.com>
2019-02-15 12:46:06 +0300

    Replace type of MongooseRunRecord from interface to class.

--- src/app/core/models/run-record.model.ts
+++ src/app/core/models/run-record.model.ts
@@ -1,10 +1,8 @@
 import { RunDuration } from '../run-duration';
 import { MongooseRunStatus } from '../mongoose-run-status';
 
-export interface MongooseRunRecord { 
-    status: MongooseRunStatus; 
-    startTime: String;
-    Nodes: String[]; 
-    Duration: RunDuration; 
-    comment: String;
+export class MongooseRunRecord { 
+
+    constructor(status: MongooseRunStatus,  startTime: String, Nodes: String[],  duration: RunDuration, comment: String) { }
+
 }
