830cf974ebbeae04c94b4ea80ff496128ece964e
Andrey Koltsov <a99920641@gmail.com>
2019-02-15 16:58:27 +0300

    Add separate component for Mongoose Run status icon.

---
+++ src/app/mongoose-run-status-icon/mongoose-run-status-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mongoose-run-status-icon',
+  templateUrl: './mongoose-run-status-icon.component.html',
+  styleUrls: ['./mongoose-run-status-icon.component.css']
+})
+export class MongooseRunStatusIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
