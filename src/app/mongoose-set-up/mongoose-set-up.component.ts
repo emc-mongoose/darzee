c4b973ee91b2e7f999364485902e4e33e1abd4d8
Andrey Koltsov <a99920641@gmail.com>
2019-02-18 16:25:32 +0300

    Add observer for 'Continue' button click.

--- src/app/mongoose-set-up/mongoose-set-up.component.ts
+++ src/app/mongoose-set-up/mongoose-set-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mongoose-set-up',
@@ -7,31 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MongooseSetUpComponent implements OnInit {
 
+  readonly BASE_URL = "/setup";
+
   readonly setUpSteps = [
-    {stepName: 'Nodes', url: '/runs'},
-    {stepName: 'Configuration', url: '/nodes'},
-    {stepName: 'Scenario', url: '/control'}
+    {stepName: 'Nodes', url: 'runs'},
+    {stepName: 'Configuration', url: 'nodes'},
+    {stepName: 'Scenario', url: 'control'}
   ]
 
-  currentStepNumber: number = 1;
+  currentStepNumber: number = 0;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   // MARK: - Lifecycle 
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.navigate([this.BASE_URL, this.setUpSteps[this.currentStepNumber].url]);
+  }
 
   // MARK: - Public 
 
   getComplitionPercentage(): number { 
     // NOTE: 'rawPercentage' is percents represented in decimal values (e.g.: 0.3, etc.).
-    let rawPercentage = (this.currentStepNumber / this.setUpSteps.length);
+    let rawPercentage = ((this.currentStepNumber + 1) / this.setUpSteps.length);
     return rawPercentage * 100;
   }
 
   getCurrentStepName(): string { 
     // '1' is subbsracted in order to match array's number system which starts from zero.
-    return this.setUpSteps[this.currentStepNumber - 1].stepName;
+    return this.setUpSteps[this.currentStepNumber].stepName;
+  }
+
+  onNextStepClicked() { 
+    this.currentStepNumber++; 
+    this.router.navigate([this.BASE_URL, this.setUpSteps[this.currentStepNumber].url]);
   }
 
 }
