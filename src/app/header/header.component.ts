6737bac88631d96c94ec8a55f8059ba56910d808
Andrey Koltsov <a99920641@gmail.com>
2019-02-18 14:37:53 +0300

    Add redirecting from 'Runs' button to runs page.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,7 @@ export class HeaderComponent implements OnInit {
 
   readonly buttonRoutes = [
     // TODO: Add actual links to the pages once they'd be created
-    {linkName: 'Runs', url: '/'},
+    {linkName: 'Runs', url: '/runs'},
     {linkName: 'New run', url: '/'},
     {linkName: 'Scenarios', url: '/'},
     {linkName: 'Create scenario', url: '/'}
