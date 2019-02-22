347b7dd83b9123f8757b78f20a9710e5269778ae
Andrey Koltsov <a99920641@gmail.com>
2019-02-22 20:36:27 +0300

    Change links from string literals to RoutesList class variables @ Router.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,7 @@
-export class Router { 
+export class RoutesList { 
     public static readonly MONGOOSE_SETUP = "setup";
     public static readonly NODES = "nodes";
     public static readonly RUNS = "runs";
     public static readonly SCENARIO = "control";
     public static readonly MONGOOSE_COMFIGURATION = "configuration-editing.component";
-
 }
