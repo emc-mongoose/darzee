9417b4c7c6610ac444ab988a945a8fc42c4872f6
Andrey Koltsov <a99920641@gmail.com>
2019-02-22 13:46:27 +0300

    Remove deprecated navbar from the related pages.

--- src/app/set-up-steps/contorl-page/contorl-page.module.ts
+++ src/app/set-up-steps/contorl-page/contorl-page.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ControlPageRootComponent } from './control-page-root/control-page-root.component';
-import { ControlNavbarComponent } from './control-navbar/control-navbar.component';
 import { ScenariosComponent } from './scenarios/scenarios.component';
 
 // NOTE: CodeMirror's module for code displaying
@@ -13,7 +12,7 @@ import { CodemirrorModule } from '@ctrl/ngx-codemirror';
     CommonModule,
     CodemirrorModule
   ],
-  declarations: [ControlPageRootComponent, ControlNavbarComponent, ScenariosComponent],
+  declarations: [ControlPageRootComponent, ScenariosComponent],
   // NOTE: Adding a custom elements schema in order to work with CodeMirror's custom component
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [ControlPageRootComponent]
