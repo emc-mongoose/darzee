9367806b08673c8b551c285bb40f80bd75f880c5
Andrey Koltsov <a99920641@gmail.com>
2019-02-25 18:09:55 +0300

    Remove duplicated files.

--- src/app/core/mongoose-run-status.ts
+++
@@ -1,9 +0,0 @@
-export enum MongooseRunStatus { 
-    
-    // NOTE: "All" status is used to match every existing Mongoose Run Record.
-    // It's being used in filter purposes. 
-    All = "All",
-
-    Finished = "Finished", 
-    Running = "Running",
-}
