8d84173fbeb77a170982c617d79c227d8283de10
Andrey Koltsov <a99920641@gmail.com>
2019-03-04 11:58:34 +0300

    Remove unused field @ control api service.

--- console/src/app/core/services/control-api/control-api.service.ts
+++ console/src/app/core/services/control-api/control-api.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Constants } from 'src/app/common/constants';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +13,6 @@ export class ControlApiService {
 
   mongooseSlaveNodes: String[] = [];
 
-  mongooseConfiguration: Object; 
-
   readonly HTTP_PREFIX = "http://";
   readonly JSON_CONTENT_TYPE = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -24,15 +20,15 @@ export class ControlApiService {
 
   constructor(private http: HttpClient) {
     this.getMongooseConfiguration(Constants.Configuration.MONGOOSE_HOST_IP);
-   }
+  }
 
   // MARK: - Public
 
-  runMongoose(jsonConfiguration: Object = "", javaScriptScenario: String = ""): any {
+  runMongoose(jsonConfiguration: Object, javaScriptScenario: String = ""): any {
     // NOTE: Using JSON.stirngly(...) to pass Scenario as a HTTP parameter. It could contains multiple quotes, JSON.stringfy(...) handles it well. 
     javaScriptScenario = JSON.stringify(javaScriptScenario);
 
-    let formData = new FormData(); 
+    let formData = new FormData();
     formData.append('defaults', jsonConfiguration.toString());
 
     this.http.post(this.HTTP_PREFIX + Constants.Configuration.MONGOOSE_HOST_IP + '/run?defaults=' + formData + "&scenario=" + javaScriptScenario, this.getHttpHeaderForJsonFile()).subscribe(
@@ -42,7 +38,7 @@ export class ControlApiService {
   }
 
   // NOTE: Returning Mongoose configuration as JSON 
-  getMongooseConfiguration(mongooseHostIp: string): any { 
+  getMongooseConfiguration(mongooseHostIp: string): any {
     const configEndpoint = "/config";
     return this.http.get(this.HTTP_PREFIX + mongooseHostIp + configEndpoint, this.JSON_CONTENT_TYPE);
   }
@@ -50,7 +46,7 @@ export class ControlApiService {
 
   // MARK: - Private
 
-  private getHttpHeaderForJsonFile(): HttpHeaders { 
+  private getHttpHeaderForJsonFile(): HttpHeaders {
     const httpHeadersForMongooseRun = new HttpHeaders();
     httpHeadersForMongooseRun.append('Accept', 'application/json');
     const eTag = this.getEtagForRun();
@@ -59,11 +55,10 @@ export class ControlApiService {
   }
 
   // NOTE: ETAG is HEX representation of configuration start time in milliseconds 
-  private getEtagForRun(): string { 
+  private getEtagForRun(): string {
     const currentDateTime = Date.now();
-    const hexNumericSystemBase = 16; 
+    const hexNumericSystemBase = 16;
     return currentDateTime.toString(hexNumericSystemBase);
   }
-
 
 }
