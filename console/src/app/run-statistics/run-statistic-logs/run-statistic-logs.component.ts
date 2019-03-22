030a556dc8f489be468407c7ffdbcad5b8a27ba2
Andrey Koltsov <a99920641@gmail.com>
2019-03-22 18:35:02 +0300

    Add observer on log tab click @ mongoose run logs page.

--- console/src/app/run-statistics/run-statistic-logs/run-statistic-logs.component.ts
+++ console/src/app/run-statistics/run-statistic-logs/run-statistic-logs.component.ts
@@ -12,30 +12,33 @@ export class RunStatisticLogsComponent implements OnInit {
 
   @Input() processingRunRecord: MongooseRunRecord;
 
-  private metricsName: String[] = [];
   private displayingLog = ' mongoose_duration_count{load_step_id="robotest",load_op_type="CREATE",storage_driver_limit_concurrency="1",item_data_size="1MB",start_time="1544351424363",node_list="[]",user_comment="",} 2981.0 \n mongoose_duration_sum{load_step_id="robotest",load_op_type="CREATE",storage_driver_limit_concurrency="1",item_data_size="1MB",start_time="1544351424363",node_list="[]",user_comment="",} 0.060955 mongoose_duration_count{load_step_id="robotest",load_op_type="CREATE",storage_driver_limit_concurrency="1",item_data_size="1MB",start_time="1544351424363",node_list="[]",user_comment="",} 2981.0 \n mongoose_duration_sum{load_step_id="robotest",load_op_type="CREATE",storage_driver_limit_concurrency="1",item_data_size="1MB",start_time="1544351424363",node_list="[]",user_comment="",} 0.060955 mongoose_duration_count{load_step_id="robotest",load_op_type="CREATE",storage_driver_limit_concurrency="1",item_data_size="1MB",start_time="1544351424363",node_list="[]",user_comment="",} 2981.0 \n mongoose_duration_sum{load_step_id="robotest",load_op_type="CREATE",storage_driver_limit_concurrency="1",item_data_size="1MB",start_time="1544351424363",node_list="[]",user_comment="",} 0.060955 mongoose_duration_count{load_step_id="robotest",load_op_type="CREATE",storage_driver_limit_concurrency="1",item_data_size="1MB",start_time="1544351424363",node_list="[]",user_comment="",} 2981.0 \n mongoose_duration_sum{load_step_id="robotest",load_op_type="CREATE",storage_driver_limit_concurrency="1",item_data_size="1MB",start_time="1544351424363",node_list="[]",user_comment="",} 0.060955';
-  private metricsTabs: BasicTab[] = []; 
+  private logTabs: BasicTab[] = []; 
  
   constructor(private monitoringApiService: MonitoringApiService) { }
 
   // MARK: - Lifecycle
 
   ngOnInit() {
-    this.metricsName = this.monitoringApiService.getAvailableLogNames();
-    this.initMetricsTabs();
+    this.initlogTabs();
   }
 
   // MARK: - Public
 
+  changeDisplayingLog(selectedTabName) { 
+    console.log("Log has been clicked" , selectedTabName);
+  }
+
   // MARK: - Private
 
-  private initMetricsTabs() { 
-    for (let metricName of this.metricsName) { 
+  private initlogTabs() { 
+    let availableLogNames = this.monitoringApiService.getAvailableLogNames();
+    for (let logName of availableLogNames) { 
       let TAB_LINK_MOCK = "/";
-      let tab = new BasicTab(metricName, TAB_LINK_MOCK);
-      this.metricsTabs.push(tab);
+      let tab = new BasicTab(logName, TAB_LINK_MOCK);
+      this.logTabs.push(tab);
     }
-    const initialTab = this.metricsTabs[0];
+    const initialTab = this.logTabs[0];
     initialTab.isActive = true; 
   }
 }
