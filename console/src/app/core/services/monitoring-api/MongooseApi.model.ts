4b74dfb769633072c1bd7a3f7bdf4571535790b9
Andrey Koltsov <a99920641@gmail.com>
2019-03-27 21:43:27 +0300

    Add link to Mongoose API.

--- console/src/app/core/services/monitoring-api/MongooseApi.model.ts
+++ console/src/app/core/services/monitoring-api/MongooseApi.model.ts
@@ -1,4 +1,9 @@
+// A class that describes endpoints for Mongoose API. 
+// See Mongoose API (as for 27.03.2019): ...
+// ... https://github.com/emc-mongoose/mongoose-base/tree/master/doc/interfaces/api/remote
+
 export namespace MongooseApi { 
+
     export class RunApi { 
         public static readonly RUN = "/run";
     }
