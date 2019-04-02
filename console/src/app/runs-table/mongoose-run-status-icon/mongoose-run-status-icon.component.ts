ffbcda4d4b6ab7826eeb9597448b49b1d257ec2b
Andrey Koltsov <a99920641@gmail.com>
2019-04-02 16:53:18 +0300

    Add unavailable button style.

--- console/src/app/runs-table/mongoose-run-status-icon/mongoose-run-status-icon.component.ts
+++ console/src/app/runs-table/mongoose-run-status-icon/mongoose-run-status-icon.component.ts
@@ -9,45 +9,67 @@ import { MongooseRunStatus } from 'src/app/core/mongoose-run-status';
 
 export class MongooseRunStatusIconComponent implements OnInit {
 
+  // NOTE: We're using bootstrap buttons style. See: https://www.w3schools.com/bootstrap/bootstrap_buttons.asp 
+  private readonly BTN_SUCCESS_TAG = 'btn-success';
+  private readonly BTN_WARNING_TAG = 'btn-warning';
+  private readonly BTN_UNAVAILABLE_TAG = '';
+
   @Input() runStatus: MongooseRunStatus = MongooseRunStatus.Running;
   @ViewChild('resultsTag') resultsTag: ElementRef;
-  
+
   constructor() { }
 
-  ngOnInit() {  }
+  ngOnInit() { }
 
-    // MARK: - Public
+  // MARK: - Public
 
-    updateStatus(newStatus) { 
-      this.runStatus = newStatus;
+  updateStatus(newStatus) {
+    this.runStatus = newStatus;
+  }
+
+
+
+  public getMongooseRunIconClass(): String { 
+    if (this.isRunningCompleted()) { 
+      return this.BTN_SUCCESS_TAG;
     }
-
-    isRunningCompleted(): boolean { 
-      return (this.runStatus == MongooseRunStatus.Finished);
-    }
-
-    onStatusButtonClicked() { 
-      console.log("status btn clicked");
-    }
-
-    getStatusTag(): String { 
-      let actualTag = 'unavailableTag';
-      switch (this.runStatus) { 
-        case MongooseRunStatus.Unavailable: { 
-          actualTag = 'unavailableTag';
-          break;
-        }
-        case MongooseRunStatus.Running: { 
-          actualTag = 'detailsTag';
-          break;
-        }
-        case MongooseRunStatus.Finished: { 
-          actualTag = 'resultsTag';
-        }
-        default: { 
-          actualTag=  'unavailableTag';
-        }
+    switch (this.runStatus) { 
+      case MongooseRunStatus.Running: { 
+        return this.BTN_WARNING_TAG;
       }
-      return actualTag;
+      case MongooseRunStatus.Unavailable: { 
+        return this.BTN_UNAVAILABLE_TAG;
+      }
     }
+  }
+
+  onStatusButtonClicked() {
+    console.log("status btn clicked");
+  }
+
+  getStatusTag(): String {
+    let actualTag = 'unavailableTag';
+    switch (this.runStatus) {
+      case MongooseRunStatus.Unavailable: {
+        actualTag = 'unavailableTag';
+        break;
+      }
+      case MongooseRunStatus.Running: {
+        actualTag = 'detailsTag';
+        break;
+      }
+      case MongooseRunStatus.Finished: {
+        actualTag = 'resultsTag';
+      }
+      default: {
+        actualTag = 'unavailableTag';
+      }
+    }
+    return actualTag;
+  }
+
+  // MARK: - Private
+  private isRunningCompleted(): boolean {
+    return (this.runStatus == MongooseRunStatus.Finished);
+  }
 }
