7aa711a66f3ccd51d96327cb21cd929abf36620c
Andrey Koltsov <a99920641@gmail.com>
2019-04-09 17:16:17 +0300

    Add saving file via NodeJS's fs.

--- console/server/node-server.js
+++ console/server/node-server.js
@@ -1,25 +1,42 @@
 const MONGOOSE_CONSOLE_DEFAULT_PORT = 8080;
+const PROMETHEUS_CONFIGURATION_PATH = '/configuration/prometheus.yml';
+
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
 var app = express();
 
 var path = __dirname + '';
 var port = process.env.CONSOLE_PORT || MONGOOSE_CONSOLE_DEFAULT_PORT;
 
 app.use(express.static(path));
+app.use(bodyParser.json()); // NOTE: Supporting JSON-encoded bodies 
+app.use(express.multipart()); // NOTE: We're saving Prometheus configuration via the server
+
+// NOTE: Configurating server to serve index.html since during the production ...
+// build Angular converts its html's to only one file.
 app.get('*', function(req, res) {
     res.sendFile(path + '/index.html');
 });
 
-app.post('/savePrometheusConfiguration', function (req, res) {
-    var file_name = "prometheus.yml"
-    var file_content = req.body;
+app.post('/savefile', function (req, res) {
+    var fileName = req.body.fileName;
+    var fileContent = req.body.fileContent;
 
-    var stream = fs.createWriteStream(file_name);
+    var stream = fs.createWriteStream(fileName);
     stream.once('open', function () {
-        stream.write(file_content);
+        stream.write(fileContent);
         stream.end();
     });
+
+    fs.writeFile(PROMETHEUS_CONFIGURATION_PATH, fileContent, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("Prometheus configuration has been updated.");
+    }); 
 });
 
 app.listen(port);
-
