67802dea0ec12bf7c8d147b0a1adf807ced68a50
Andrey Koltsov <a99920641@gmail.com>
2019-04-13 21:41:23 +0300

    Change amount of spaces in generated .yml file.

--- console/src/app/common/FileOperations/PrometheusConfigurationEditor.ts
+++ console/src/app/common/FileOperations/PrometheusConfigurationEditor.ts
@@ -56,7 +56,7 @@ export class PrometheusConfigurationEditor {
 
     private getUpdatedTargetsValue(targets: String[]): String {
         targets = this.surroundListItemsWithCharacter(targets, this.TARGET_LIST_ELEMENTS_SURROUNDING_CHARACTERS);
-        let fieldNameAndValueDelimiter = " ";
+        let fieldNameAndValueDelimiter = "  "; // NOTE: prometheus.yml file delimiter contains 2 whitesapces
         // NOTE: Targets property and value must retain delimiter. Prometheus will crash otherwise.
         return `${this.TARGETS_PROPERTY_NAME}:${fieldNameAndValueDelimiter}[${targets}]`
     }
