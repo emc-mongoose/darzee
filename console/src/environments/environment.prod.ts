ceb722437f126f03b09e095772adb9d7da7f6271
Andrey Koltsov <a99920641@gmail.com>
2019-04-15 13:01:59 +0300

    Add configuration from .env file into environment structure.

--- console/src/environments/environment.prod.ts
+++ console/src/environments/environment.prod.ts
@@ -1,4 +1,18 @@
 export const environment = {
   production: true,
-  prometheusConfigPath: '${PROMETHEUS_CONFIGURATION_PATH}'
+
+  // NOTE: Prometheus-related configuration. It's collection metrics exported by Mongoose. 
+  prometheusConfigPath: '${PROMETHEUS_CONFIGURATION_PATH}',
+  prometheusIp: '${PROMETHEUS_IMAGE_IP}',
+  prometheusPort: '${PROMETHEUS_PORT}',
+
+  // NOTE: NodeJS Server related configuration. It's used to server .html files ...
+  // ... (that's why it's address matches Mongoose Console's address) in production and ...
+  // ... to perform POST requests to Prometheus.
+  nodeJsServerIp: '${NODE_SERVER_IMAGE_IP=}',
+  nodeJsServerPort: '${CONSOLE_PORT}',
+
+  // NOTE: Mongoose-related configuration 
+  mongooseIp: 'localhost',
+  mongoosePort: '${MONGOOSE_PORT}'
 };
--- console/src/environments/environment.ts
+++ console/src/environments/environment.ts
@@ -4,7 +4,21 @@
 
 export const environment = {
   production: false,
-  prometheusConfigPath: '/assets/configuration-examples/prometheus/prometheus.yml'
+
+  // NOTE: Prometheus-related configuration
+  prometheusConfigPath: '/assets/configuration-examples/prometheus/prometheus.yml',
+  prometheusIp: 'localhost',
+  prometheusPort: 9090,
+
+  // NOTE: NodeJS Server related configuration. It's ...
+  // ... being used to server .html files in production and perform POST ...
+  // ... requests to Prometheus.
+  nodeJsServerIp: 'localhost',
+  nodeJsServerPort: 8080,
+
+  // NOTE: Mongoose-related configuration 
+  mongooseIp: 'localhost',
+  mongoosePort: 9999
 };
 
 /*
