275e94bd153747c763c892bdf0d84135ca4310af
Andrey Koltsov <a99920641@gmail.com>
2019-04-18 13:28:22 +0300

    Add API for available nodes addition @ shared service.

--- console/src/app/core/services/mongoose-data-shared-service/mongoose-data-shared-service.service.ts
+++ console/src/app/core/services/mongoose-data-shared-service/mongoose-data-shared-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { MongooseRunNode } from '../../models/mongoose-run-node.model';
 import { environment } from 'src/environments/environment.prod';
 import { ResourceLocatorType } from '../../models/address-type';
@@ -10,18 +10,32 @@ import { ResourceLocatorType } from '../../models/address-type';
 export class MongooseDataSharedServiceService {
 
   private availableMongooseNodes$: BehaviorSubject<MongooseRunNode[]> = new BehaviorSubject<MongooseRunNode[]>([])
+  private currentAvailableMongooseNodes: MongooseRunNode[] = [];
 
   constructor() {
     this.configureDefaultMongooseRunNodes();
    }
 
+   // MARK: - Public 
+
+   public getAvailableRunNodes(): Observable<MongooseRunNode[]> { 
+     return this.availableMongooseNodes$.asObservable();
+   }
+   
+   public addMongooseRunNode(mongooseRunNode: MongooseRunNode) { 
+     this.currentAvailableMongooseNodes.push(mongooseRunNode);
+     this.availableMongooseNodes$.next(this.currentAvailableMongooseNodes);
+   }
    // MARK: - Private 
+
    private configureDefaultMongooseRunNodes() { 
      // NOTE: Default Mongoose run node is an IP address. 
      let defaultMongooseRunNodeResource = `${environment.mongooseIp}:${environment.mongoosePort}`;
     let defaultMongooseRunNode = new MongooseRunNode(defaultMongooseRunNodeResource, ResourceLocatorType.IP); 
 
     let defaultMongooseRunNodes = [defaultMongooseRunNode];
+
+    this.currentAvailableMongooseNodes = defaultMongooseRunNodes;
     this.availableMongooseNodes$.next(defaultMongooseRunNodes)
    }
 }
