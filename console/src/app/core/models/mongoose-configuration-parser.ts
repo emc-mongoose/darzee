bcb9ce8b26f889412382645ffb694a41ba6eae9e
Andrey Koltsov <a99920641@gmail.com>
2019-04-19 11:30:17 +0300

    Add Mongoose configuration parser.

---
+++ console/src/app/core/models/mongoose-configuration-parser.ts
@@ -0,0 +1,50 @@
+import { MongooseRunNode } from "./mongoose-run-node.model";
+
+export class MongooseConfigurationParser { 
+    private configuration: any; 
+
+    constructor(mongooseConfiguration: any) { 
+        this.configuration = mongooseConfiguration;
+    }
+
+    // MARK: - Public 
+
+    public getConfigurationWithAdditionalNodes(additionalNodes: MongooseRunNode[]): any { 
+        if (additionalNodes.length == 0) { 
+            return additionalNodes; 
+        }
+        if (!this.isSlaveNodesFieldExisInConfiguration(additionalNodes)) { 
+            throw new Error(`Invalid Mongoose configuration structure.`);
+        }
+        
+        var existingNodesInConfiguration = this.getNodesFromConfiguration(this.configuration);
+
+        additionalNodes.forEach(node => { 
+            let nodeAddress = node.getResourceLocation();
+            existingNodesInConfiguration.push(nodeAddress);
+        })
+
+        this.configuration.load.step.node.addrs = existingNodesInConfiguration;
+    }
+
+    // MARK: - Private 
+
+    private isSlaveNodesFieldExisInConfiguration(configuration: any): boolean {
+        // NOTE: Check if 'Address' field exists on received Mongoose JSON configuration. 
+        // As for 04.03.2019, it's located at load -> step -> node -> addrs
+        return !((configuration.load == undefined) &&
+          (configuration.load.step == undefined) &&
+          (configuration.load.step.node == undefined) &&
+          (configuration.load.step.node.addrs == undefined));
+      }
+      
+      private getNodesFromConfiguration(configuration: any): string[] { 
+        // NOTE: Returning slave nodes as string array since we're retrieving ...
+        // ... it from JSON. As for now, we don't care about its resource locator ...
+        // ... type.   
+        if (!this.isSlaveNodesFieldExisInConfiguration(configuration)) {
+              throw new Error(`Required nodes field doesn't exist in Mongoose configuration.`)
+          }
+          return configuration.load.step.node.addrs;
+      }
+}
