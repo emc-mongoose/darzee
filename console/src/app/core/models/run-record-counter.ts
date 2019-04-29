21072d4d90be8f6028daff3b0a497a46e68d6ab1
Andrey Koltsov <a99920641@gmail.com>
2019-04-29 17:56:50 +0300

    Remove deprecated method.

--- console/src/app/core/models/run-record-counter.ts
+++ console/src/app/core/models/run-record-counter.ts
@@ -4,22 +4,8 @@ import { Observable, forkJoin, of } from "rxjs";
 import { map } from "rxjs/operators";
 
 export class MongooseRunRecordCounter { 
-    constructor() {}
 
-    public countRecordsByStatus(records: MongooseRunRecord[], requiredStatus: string): Number { 
-        if (requiredStatus == MongooseRunStatus.All) { 
-            return records.length;
-        }
-        
-        let amountOfRecords = 0; 
-        records.forEach(record => { 
-            console.log(`Кecord.getStatus(): ${record.getStatus()}`)
-            if (record.getStatus() == requiredStatus) { 
-                amountOfRecords++;
-            }
-        });
-        return amountOfRecords;
-    }
+    constructor() {}
 
     public getAmountOfRecordsWithStatus$(records: MongooseRunRecord[], requiredStatus: string): Observable<Number> { 
         if (requiredStatus == MongooseRunStatus.All) { 
