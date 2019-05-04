d636ce25841d4f8e6cbf6ff025dc0a37fc3a151a
Andrey Koltsov <a99920641@gmail.com>
2019-05-04 15:51:12 +0300

    Add necessary field & method to mongoose chart inteface.

--- console/src/app/core/models/chart/mongoose-chart.interface.ts
+++ console/src/app/core/models/chart/mongoose-chart.interface.ts
@@ -1,10 +1,14 @@
 import { MongooseChartDataset } from "./mongoose-chart-dataset.model";
 import { MongooseChartOptions } from "./mongoose-chart-options";
+import { MongooseChartDao } from "./mongoose-chart-dao.mode";
 
-export interface MongooseChart { 
-chartOptions: MongooseChartOptions;
-chartLabels: string[];
-chartType: string;
-chartLegend: boolean;
-chartData: MongooseChartDataset; 
+export interface MongooseChart {
+    chartOptions: MongooseChartOptions;
+    chartLabels: string[];
+    chartType: string;
+    chartLegend: boolean;
+    chartData: MongooseChartDataset;
+
+    mongooseChartDao: MongooseChartDao; 
+    updateChart(); 
 }
