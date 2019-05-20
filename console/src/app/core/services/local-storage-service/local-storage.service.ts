a514e614c7a6aa6e953bbfbc16eb2d9888977c4a
Andrey Koltsov <a99920641@gmail.com>
2019-05-20 17:12:09 +0300

    Remove unnecessary console output.

--- console/src/app/core/services/local-storage-service/local-storage.service.ts
+++ console/src/app/core/services/local-storage-service/local-storage.service.ts
@@ -7,20 +7,19 @@ import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 })
 export class LocalStorageService {
 
-  readonly STORAGE_KEY = "mongoose-darzee";
+  readonly ENTRY_NODE_TO_RUN_ID_MAP_STORAGE_KEY = "mongoose-darzee-entry-node-to-run-id-map";
 
 
   constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
 
   public saveToLocalStorage(runEntryNode: string, runId: string) { 
-    const currentEntryNodeMap = this.storage.get(this.STORAGE_KEY) || [];
-    console.log(`currentEntryNodeMap: ${JSON.stringify(currentEntryNodeMap)}`);
+    const currentEntryNodeMap = this.storage.get(this.ENTRY_NODE_TO_RUN_ID_MAP_STORAGE_KEY) || [];
     currentEntryNodeMap.push({
       runEntryNode: `${runEntryNode}`,
       runId: `${runId}`
     });
 
-    this.storage.set(this.STORAGE_KEY, currentEntryNodeMap);
+    this.storage.set(this.ENTRY_NODE_TO_RUN_ID_MAP_STORAGE_KEY, currentEntryNodeMap);
     console.log(`updated currentEntryNodeMap: ${JSON.stringify(currentEntryNodeMap)}`);
   }
 }
