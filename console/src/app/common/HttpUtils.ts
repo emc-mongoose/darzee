4cf61e4273534a9f3254e3121e75f8be4cf3de5d
Andrey Koltsov <a99920641@gmail.com>
2019-05-24 20:08:27 +0300

    Update ip validation function @ Http utils.

--- console/src/app/common/HttpUtils.ts
+++ console/src/app/common/HttpUtils.ts
@@ -1,9 +1,29 @@
 export class HttpUtils {
 
+    public static readonly PORT_NUMBER_UPPER_BOUND: number = 65535;
+    public static readonly LOCALHOST_KEYWORD: string = "localhost";
+
     // NOTE: Checks if string matches an IP by pattern.
     public static isIpAddressValid(ipAddress: string): boolean {
-        let ipValidationRegex: RegExp = new RegExp(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/);
-        return ipValidationRegex.test(ipAddress);
+        const localhostKeyword: string = HttpUtils.LOCALHOST_KEYWORD;
+        const hasLocalhostKeyword: boolean = ipAddress.includes(localhostKeyword);
+        if (!hasLocalhostKeyword) {
+            let ipValidationRegex: RegExp = new RegExp(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/);
+            return ipValidationRegex.test(ipAddress);
+        }
+        const emptyString = "";
+        const portNumberAndKeywordDelimiter = ":";
+        const remaningAddressWithoutKeywords = ipAddress.replace(localhostKeyword + portNumberAndKeywordDelimiter, emptyString);
+        
+        const maximumAmountOfDigitsInPort = 5; 
+        if (remaningAddressWithoutKeywords.length < maximumAmountOfDigitsInPort) { 
+            return false; 
+        }
+
+        const portNumber = Number(remaningAddressWithoutKeywords);
+        
+        return (isNaN(portNumber) && (portNumber <= HttpUtils.PORT_NUMBER_UPPER_BOUND));
+
     }
 
     public static shouldPerformMongooseRunRequest(mongooseAddress: string): boolean {
@@ -12,5 +32,5 @@ export class HttpUtils {
         let isIpPointsToLocalhost: boolean = mongooseAddress.includes("localhost");
         return ((isIpValid) || (isIpPointsToLocalhost));
     }
-å
+    å
 }
