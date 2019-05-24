e7d053963153e3bc7be957b86f95dd5e95c154b2
Andrey Koltsov <a99920641@gmail.com>
2019-05-24 16:11:25 +0300

    Add subscription to possible Prometheus nodes.

--- console/src/app/modules/app-module/components/common/prometheus-error/prometheus-error.component.ts
+++ console/src/app/modules/app-module/components/common/prometheus-error/prometheus-error.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Subject, Observable, merge } from 'rxjs';
+import { Subject, Observable, merge, Subscription } from 'rxjs';
 import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
 import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { MongooseDataSharedServiceService } from 'src/app/core/services/mongoose-data-shared-service/mongoose-data-shared-service.service';
+import { MongooseRunNode } from 'src/app/core/models/mongoose-run-node.model';
 
 @Component({
   selector: 'app-prometheus-error',
@@ -14,21 +16,26 @@ export class PrometheusErrorComponent implements OnInit {
 
   @ViewChild(`prometheusAddressTypeheadInstance`) prometheusAddressTypeheadInstance: NgbTypeahead;
 
+  private DEFAULT_PROMETHEUS_NODE_ADDRESS = "localhost";
+  private DEFAULT_PROMETHEUS_NODE_PORT = "9090";
+
   public prometheusResourceLocation: string = "";
   public currentEnteredText: string = "";
   public focus$ = new Subject<string>();
   public click$ = new Subject<string>();
 
+  private possiblePrometheusNodesList: string[] = [];
+  private activeSubscriptions: Subscription = new Subscription();
 
-  private possiblePrometheusNodesList: string[] = ['localhost:9090'];
-
-  constructor() {
+  constructor(private mongooseDataSharedServiceService: MongooseDataSharedServiceService) {
     let prometheusBaseAddress: string = environment.prometheusIp;
     let prometheusPort: string = environment.prometheusPort;
     this.prometheusResourceLocation = `${prometheusBaseAddress}:${prometheusPort}`;
   }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.subscribeToPossiblePrometheusRunNodes();
+  }
 
   search = (enteringText$: Observable<string>) => {
     if (this == undefined) {
@@ -44,6 +51,24 @@ export class PrometheusErrorComponent implements OnInit {
         return (term === '' ? this.possiblePrometheusNodesList
           : this.possiblePrometheusNodesList.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10);
       })
+    )
+  }
+
+  // MARK: - Private 
+  private subscribeToPossiblePrometheusRunNodes() { 
+    this.activeSubscriptions.add(
+      this.mongooseDataSharedServiceService.getAvailableRunNodes().subscribe(
+        (availableRunNodes: MongooseRunNode[]) => {
+          let availableRunNodeAddressess: String[] = [];
+          availableRunNodes.forEach((runNode: MongooseRunNode) => {
+            availableRunNodeAddressess.push(runNode.getResourceLocation());
+          });
+          // NODE: Adding default Prometheus address.
+          let defaultPrometheusAddress = `${this.DEFAULT_PROMETHEUS_NODE_ADDRESS}:${this.DEFAULT_PROMETHEUS_NODE_PORT}`;
+          availableRunNodeAddressess.push(defaultPrometheusAddress);
+          this.possiblePrometheusNodesList = availableRunNodeAddressess as string[];
+        }
+      )
     )
   }
 
