bb6fc56c4ad5a7538625e873ccb490dd5808fafe
Andrey Koltsov <a99920641@gmail.com>
2019-05-24 16:05:02 +0300

    Add typehead for Prometheus address.

--- console/src/app/modules/app-module/components/common/prometheus-error/prometheus-error.component.ts
+++ console/src/app/modules/app-module/components/common/prometheus-error/prometheus-error.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Subject, Observable, merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-prometheus-error',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrometheusErrorComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  @ViewChild(`prometheusAddressTypeheadInstance`) prometheusAddressTypeheadInstance: NgbTypeahead;
+
+  public prometheusResourceLocation: string = "";
+  public currentEnteredText: string = "";
+  public focus$ = new Subject<string>();
+  public click$ = new Subject<string>();
+
+
+  private possiblePrometheusNodesList: string[] = ['localhost:9090'];
+
+  constructor() {
+    let prometheusBaseAddress: string = environment.prometheusIp;
+    let prometheusPort: string = environment.prometheusPort;
+    this.prometheusResourceLocation = `${prometheusBaseAddress}:${prometheusPort}`;
+  }
+
+  ngOnInit() { }
+
+  search = (enteringText$: Observable<string>) => {
+    if (this == undefined) {
+      return;
+    }
+    const debouncedText$ = enteringText$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.prometheusAddressTypeheadInstance.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => {
+        this.currentEnteredText = term;
+        return (term === '' ? this.possiblePrometheusNodesList
+          : this.possiblePrometheusNodesList.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10);
+      })
+    )
   }
 
 }
