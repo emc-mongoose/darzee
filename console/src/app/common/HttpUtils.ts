c03edbb3476cac365603c787aa897a548fccca59
Andrey Koltsov <a99920641@gmail.com>
2019-05-26 19:25:27 +0300

    Add documentzation to Http utils.

--- console/src/app/common/HttpUtils.ts
+++ console/src/app/common/HttpUtils.ts
@@ -1,14 +1,26 @@
+/**
+ * Utilities for working with HTTP calls. Mainly validation functions.
+ */
 export class HttpUtils {
 
+    /**
+     * @param PORT_NUMBER_UPPER_BOUND maximal available number of port.
+     * @param LOCALHOST_KEYWORD contains "localhost" as string.  
+     */
     public static readonly PORT_NUMBER_UPPER_BOUND: number = 65535;
     public static readonly LOCALHOST_KEYWORD: string = "localhost";
 
-    // NOTE: Checks if string matches an IP by pattern.
+    /**
+     * Returns true if @param ipAddress matches IP address pattern. 
+     * With exception if address caontains keyword (only "localhost" at the moment).
+     */
     public static isIpAddressValid(ipAddress: string): boolean {
         const localhostKeyword: string = HttpUtils.LOCALHOST_KEYWORD;
         const hasLocalhostKeyword: boolean = ipAddress.includes(localhostKeyword);
         if (!hasLocalhostKeyword) {
+            console.error(`${ipAddress} doesn't contain any key words and will be trated as an IP address to be checked.`)
             let ipValidationRegex: RegExp = new RegExp(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/);
+            console.log(`But is it valid? ${ipValidationRegex.test(ipAddress)}`)
             return ipValidationRegex.test(ipAddress);
         }
         const emptyString = "";
@@ -25,11 +37,14 @@ export class HttpUtils {
 
     }
 
+    /**
+     * @returns if given adress is valid for performing Mongoose run.
+     * @param mongooseAddress mongoose run entry node.
+     */
     public static shouldPerformMongooseRunRequest(mongooseAddress: string): boolean {
         // NOTE: Temporarily checking only IP address.
         let isIpValid: boolean = HttpUtils.isIpAddressValid(mongooseAddress);
         let isIpPointsToLocalhost: boolean = mongooseAddress.includes("localhost");
         return ((isIpValid) || (isIpPointsToLocalhost));
     }
-    å
 }
