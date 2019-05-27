7990418e2ccbe438cc252aef4801c251e7b7fa19
Andrey Koltsov <a99920641@gmail.com>
2019-05-27 15:19:20 +0300

    Add API to set chart type.

--- console/src/app/core/models/chart/mongoose-chart-interface/mongoose-chart-options.ts
+++ console/src/app/core/models/chart/mongoose-chart-interface/mongoose-chart-options.ts
@@ -1,5 +1,12 @@
+/**
+ * Specifies options for BasicChart drawn via ChartJS library.
+ */
 export class MongooseChartOptions {
 
+    /**
+     * @param CHART_DEFAULT_TYPE specifies default type of chart drawn via ChartJS library. "Linear" is a default value.
+     */
+    public static readonly CHART_DEFAULT_TYPE = "linear";
     // NOTE: Fields are public since they should match ng-chart2 library naming 
     // link: https://github.com/valor-software/ng2-charts
     public scaleShowVerticalLines: boolean = false;
@@ -7,10 +14,14 @@ export class MongooseChartOptions {
     public responsiveAnimationDuration: number = 0;
     public animation: any = {
         duration: 0
-    }
+    };
+    
     public scales: any = {
         yAxes: [{
             type: 'logarithmic'
+        }],
+        xAxes: [{
+            type: "linear"
         }]
     }
 
@@ -18,4 +29,31 @@ export class MongooseChartOptions {
         this.scaleShowVerticalLines = shouldScaleShowVerticalLines;
         this.responsive = isResponsive;
     }
+
+    /**
+     * 
+     * @param type specified the type of a certain @param axis.
+     * @throws Error if non-existing @param axis has been passed as an argument.
+     */
+    public setAxisChartType(type: string, axis: MongooseChartAxesType) { 
+        switch (axis) { 
+            case (MongooseChartAxesType.X): { 
+                this.scales.xAxes.type = type; 
+                break;
+            }
+            case (MongooseChartAxesType.Y): { 
+                this.scales.yAxes.type = type; 
+                break;
+            }
+            default: { 
+                throw new Error(`requested axis "${axis}" hasn't been found.`);
+            }
+        }
+        
+    }
+}
+
+export enum MongooseChartAxesType { 
+    X = "x",
+    Y = "y"
 }
