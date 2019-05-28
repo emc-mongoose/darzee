5966345c22efc74bbfd5d6d47b52a120d88ef297
Andrey Koltsov <a99920641@gmail.com>
2019-05-28 16:45:59 +0300

    Add API for correct chart update @ Mongoose Throughtput chart.

--- console/src/app/core/models/chart/throughput/mongoose-throughput-chart.model.ts
+++ console/src/app/core/models/chart/throughput/mongoose-throughput-chart.model.ts
@@ -5,12 +5,18 @@ import { MongooseChartDao } from "../mongoose-chart-interface/mongoose-chart-dao
 import { formatDate } from "@angular/common";
 import { MongooseMetric } from "../mongoose-metric.model";
 import { InternalMetricNames } from "../internal-metric-names";
+import { MongooseMetrics } from "src/app/core/services/mongoose-api-models/MongooseMetrics";
+import { NumbericMetricValueType } from "../mongoose-chart-interface/numeric-metric-value-type";
 
 export class MongooseThroughputChart implements MongooseChart {
 
 
-    private readonly SUCCESSFUL_OPERATIONS_DATASET_INDEX = 0;
-    private readonly FAILED_OPERATIONS_DATASET_INDEX = 1;
+    private readonly SUCCESSFUL_OPERATIONS_MEAN_DATASET_INDEX = 0;
+    private readonly SUCCESSFUL_OPERATIONS_LAST_DATASET_INDEX = 1;
+
+    private readonly FAILED_OPERATIONS_MEAN_DATASET_INDEX = 3;
+    private readonly FAILED_OPERATIONS_LAST_DATASET_INDEX = 4;
+
 
     chartOptions: MongooseChartOptions;
     chartLabels: string[];
@@ -31,38 +37,64 @@ export class MongooseThroughputChart implements MongooseChart {
         this.isChartDataValid = true;
         this.shouldShiftChart = shouldShiftChart; 
 
-        let successfulOperationsDataset = new MongooseChartDataset([], 'Successful operations');
-        let failedOperationsDataset = new MongooseChartDataset([], 'Failed operations');
+        let successfulOperationsMeanDataset = new MongooseChartDataset([], 'Successful operations, mean');
+        let successfulOperationsLastDataset = new MongooseChartDataset([], 'Successful operations, last');
 
-        this.chartData = [successfulOperationsDataset, failedOperationsDataset];
+        let failedOperationsMeanDataset = new MongooseChartDataset([], 'Failed operations, last');
+        let failedOperationsLastDataset = new MongooseChartDataset([], 'Failed operations, mean');
+
+        this.chartData = [successfulOperationsMeanDataset, successfulOperationsLastDataset, failedOperationsMeanDataset, failedOperationsLastDataset];
     }
 
+    /**
+     * Updated chart that contains successful and failed operations rate.
+     * @param recordLoadStepId 
+     * @param metrics Metrics array with both successful and failed operations, both mean and last. 
+     */
     updateChart(recordLoadStepId: string, metrics: MongooseMetric[]) {
-
-        let successfulOperationsMetricName = InternalMetricNames.SUCCESSFUL_OPERATIONS;
-        let failedOperationsMetricName = InternalMetricNames.FAILED_OPERATIONS;
-
         var throughtputChartTimestamps: string[] = [];
-        var failedOperationsMetrics: string[] = []; 
-        var successfulOperationsMetrics: string[] = [];
+
+        var failedOperationsMetricsMean: string[] = []; 
+        var failedOperationsMetricsLast: string[] = []; 
+
+        var successfulOperationsMetricsMean: string[] = [];
+        var successfulOperationsMetricsLast: string[] = [];
 
         metrics.forEach((metric: MongooseMetric) => { 
-            switch(metric.getName()) { 
-                case successfulOperationsMetricName: { 
-                    successfulOperationsMetrics.push(metric.getValue());
+            const metricValue = metric.getValue();
+            const metricName = metric.getName();
+
+            switch(metricName) { 
+                case InternalMetricNames.SUCCESSFUL_OPERATIONS_MEAN: { 
+                    successfulOperationsMetricsMean.push(metricValue);
                     throughtputChartTimestamps.push(formatDate(Math.round(metric.getTimestamp() * 1000), 'mediumTime', 'en-US'));
                     break;
                 }
-                case failedOperationsMetricName: { 
-                    failedOperationsMetrics.push(metric.getValue());
+                case InternalMetricNames.SUCCESSFUL_OPERATIONS_LAST: { 
+                    successfulOperationsMetricsLast.push(metricValue);
+                    break;
+                }
+                case InternalMetricNames.FAILED_OPERATIONS_MEAN: { 
+                    failedOperationsMetricsMean.push(metricValue);
+                    break;
+                }
+                case InternalMetricNames.FAILED_OPERATIONS_LAST: { 
+                    failedOperationsMetricsLast.push(metricValue);
+                    break;
+                }
+                default: { 
+                    console.error(`Internal metricname hasn't been found for metric ${metricName}`);
                     break;
                 }
             }
         })
         this.chartLabels = throughtputChartTimestamps; 
-        this.chartData[this.SUCCESSFUL_OPERATIONS_DATASET_INDEX].setChartData(successfulOperationsMetrics);
-        this.chartData[this.FAILED_OPERATIONS_DATASET_INDEX].setChartData(failedOperationsMetrics);
 
+        this.updateChartData(successfulOperationsMetricsMean, InternalMetricNames.SUCCESSFUL_OPERATIONS_MEAN);
+        this.updateChartData(successfulOperationsMetricsLast, InternalMetricNames.SUCCESSFUL_OPERATIONS_LAST);
+
+        this.updateChartData(failedOperationsMetricsMean, InternalMetricNames.FAILED_OPERATIONS_MEAN);
+        this.updateChartData(failedOperationsMetricsLast, InternalMetricNames.FAILED_OPERATIONS_LAST);
     }
 
     shouldDrawChart(): boolean {
@@ -72,6 +104,38 @@ export class MongooseThroughputChart implements MongooseChart {
     private shouldShift(): boolean {
         const maxAmountOfPointsInGraph = 20;
         return (this.chartLabels.length >= maxAmountOfPointsInGraph);
+    }
+
+    /**
+     * Updates specific chart with @param metrics values.
+     * 
+     * @param numericMetricValueType - MEAN or LAST successful / faield operations amount.
+     */
+    private updateChartData(metricVakyes: string[], metricName: string) { 
+        let relatedChartIndex: number = undefined; 
+        switch(metricName) { 
+            case InternalMetricNames.SUCCESSFUL_OPERATIONS_MEAN: { 
+                relatedChartIndex = this.SUCCESSFUL_OPERATIONS_MEAN_DATASET_INDEX;
+                break;
+            }
+            case InternalMetricNames.SUCCESSFUL_OPERATIONS_LAST: { 
+                relatedChartIndex = this.SUCCESSFUL_OPERATIONS_LAST_DATASET_INDEX;
+                break;
+            }
+            case InternalMetricNames.FAILED_OPERATIONS_MEAN: { 
+                relatedChartIndex = this.FAILED_OPERATIONS_MEAN_DATASET_INDEX;
+                break;
+            }
+            case InternalMetricNames.FAILED_OPERATIONS_LAST: { 
+                relatedChartIndex = this.FAILED_OPERATIONS_LAST_DATASET_INDEX;
+                break;
+            }
+            default: { 
+                console.error(`Unable to find chart index for thoughtput metric ${metricName}`);
+                break;
+            }
+        }
+        this.chartData[relatedChartIndex].setChartData(metrics);
     }
 
 }
