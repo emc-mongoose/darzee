7ed48634fe8e56664b8749190e628276b47b55fc
Andrey Koltsov <a99920641@gmail.com>
2019-06-17 16:39:37 +0300

    Add lifecycle enabling to Prometheus.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # NOTE: The file contains script for starting up Darzee's supporting services.
 # NOTE Starting Prometheus in background 
-nohup /./prometheus/bin/prometheus --config.file=/prometheus/etc/prometheus/prometheus.yml 2> prometheus_logs.txt &
+nohup /./prometheus/bin/prometheus --config.file=/prometheus/prometheus/server/prometheus.yml --web.enable-lifecycle 2> prometheus_logs.txt &
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start Prometheus service: $status"
@@ -10,10 +10,10 @@ if [ $status -ne 0 ]; then
 fi
 
 # NOTE Starting NodeJS server in background 
-nohup node prometheus/prometheus/server/node-server.js >>/dev/null 2>>/dev/null &
+nohup node prometheus/prometheus/server/node-server.js 2> server_logs.txt &
 
 while true; do
-  ps aux |grep prometheus
+  ps aux |grep prometheus 2> dev/null &
 done
 
 echo "Darzee has started."
