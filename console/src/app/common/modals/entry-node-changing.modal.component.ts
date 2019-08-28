09e12495624040baf8b63be404d0e589400f6c2b
Andrey Koltsov <a99920641@gmail.com>
2019-08-28 12:52:54 +0300

    Add new badge for table row. It's now possible to select it via the UI.

--- console/src/app/common/modals/entry-node-changing.modal.component.ts
+++ console/src/app/common/modals/entry-node-changing.modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MongooseRunNode } from 'src/app/core/models/mongoose-run-node.model';
 import { MongooseSetUpService } from 'src/app/core/services/mongoose-set-up-service/mongoose-set-up.service';
@@ -13,6 +13,13 @@ export class EntryNodeChangingModalComponent {
 
   @Input() nodes: MongooseRunNode[];
 
+  @ViewChild('halfTransparentBadge') halfTransparentBadge: TemplateRef<any>;
+  @ViewChild('entryNodeBadge') entryNodeBadge: TemplateRef<any>;
+  @ViewChild('selectedEntryNodeBadge') selectedEntryNodeBadge: TemplateRef<any>;
+
+  private currentHoveringNodeLocation: string = "";
+  private updatedEntryNode: MongooseRunNode;
+
   constructor(
     public activeModal: NgbActiveModal,
     private mongooseSetUpService: MongooseSetUpService) {
@@ -22,15 +29,48 @@ export class EntryNodeChangingModalComponent {
    * Handles event when mouse is over @param node's row. 
    */
   public onMouseOverTableRow(node: MongooseRunNode): void { 
+    this.currentHoveringNodeLocation = node.getResourceLocation();
     console.log(`mouse has entered the row. Node: ${node.getResourceLocation()}`)
   }
 
+  public onMouseOutTableRow(node: MongooseRunNode): void { 
+    this.currentHoveringNodeLocation = "";
+  }
+
   public onRowClicked(node: MongooseRunNode): void { 
+    this.updatedEntryNode = node; 
     console.log(`User has clicked ${node.getResourceLocation()} row.`);
   }
 
   public isEntryNode(node: MongooseRunNode): boolean { 
     const entryNode: MongooseRunNode = this.nodes[0];
     return (entryNode.getResourceLocation() == node.getResourceLocation());
+  }
+
+  public getClassForTableRowRepresentingNode(node: MongooseRunNode): string { 
+    if (this.isEntryNode(node)) { 
+      return "table-danger";
+    }
+    return "";
+  }
+
+
+  public getTemplateForRow(node: MongooseRunNode): TemplateRef<any> { 
+    if (this.updatedEntryNode != undefined) {
+      console.log(`this.updatedEntryNode.getResourceLocation(): ${this.updatedEntryNode.getResourceLocation()}`)
+      console.log(`Current resource location: ${node.getResourceLocation()}`)
+      const isUpdatedTableRow: boolean = (this.updatedEntryNode.getResourceLocation() == node.getResourceLocation());
+      if (isUpdatedTableRow) { 
+        return this.selectedEntryNodeBadge;
+      }
+    }
+    
+    const isHovering: boolean = (this.currentHoveringNodeLocation == node.getResourceLocation());
+    if (this.isEntryNode(node)) { 
+      return this.entryNodeBadge;
+    }
+    if (isHovering) { 
+      return this.halfTransparentBadge;
+    }
   }
 }
